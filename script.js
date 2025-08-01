b9f55e9300c5e29398a5902198e4beb5ade4da86
ArenaMendrika <hei.arena.3@gmail.com>
2025-08-02 01:27:09 +0300

    feat: responsive

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-// script.js
-window.addEventListener('scroll', () => {
+window.addEventListener('DOMContentLoaded', () => {
   const nav = document.querySelector('nav');
-  if (window.scrollY > 50) {
-    nav.classList.add('scrolled');
-  } else {
-    nav.classList.remove('scrolled');
-  }
-});
 
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+      nav.classList.add('scrolled');
+    } else {
+      nav.classList.remove('scrolled');
+    }
+  });
+
+  const navToggle = document.querySelector('.nav-toggle');
+  const navLinks = document.querySelector('.nav-links');
+
+  navToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('open');
+    navToggle.classList.toggle('open');
+  });
+});
