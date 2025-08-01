c64e23eeff3f1d8d9a42bfbf8ab89f33352c579b
ArenaMendrika <hei.arena.3@gmail.com>
2025-08-02 00:53:48 +0300

    feat: footer and scrolled nav

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+// script.js
+window.addEventListener('scroll', () => {
+  const nav = document.querySelector('nav');
+  if (window.scrollY > 50) {
+    nav.classList.add('scrolled');
+  } else {
+    nav.classList.remove('scrolled');
+  }
+});
 
